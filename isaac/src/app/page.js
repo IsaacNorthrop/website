2837e62938104d479f22de685b74e6c4e0baae88
gsnap88 <isaac.northrop88@gmail.com>
2023-09-15 17:47:21 -0700

    initial commit

---
+++ isaac/src/app/page.js
@@ -0,0 +1,16 @@
+import Menu from './menu.js';
+import Projects from './projects.js';
+import Header from './header.js';
+
+
+
+
+export default function Home() {
+  return (
+    <main>
+      <Menu />
+      <Header />
+      <Projects />
+    </main>
+  )
+}
